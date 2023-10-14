8c52d6ced24bc1758d8f6e2924628398cc61c70c
Dhananjay-12 <pantpriyanshu31@gmail.com>
2023-10-14 18:39:03 +0530

    bug fixes

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -14,7 +14,7 @@ function Features() {
         </p>
         <div className="mt-4 grid grid-cols-2">
           <div className="flex items-center justify-center w-[16rem] m-4">
-            <img src="public/search.svg" alt="" className="w-12 h-12 mr-2" />
+            <img src="search.svg" alt="" className="w-12 h-12 mr-2" />
             <p>
               <span className="font-semibold">SEARCH</span> for vital company
               information
@@ -22,7 +22,7 @@ function Features() {
           </div>
 
           <div className="flex items-center justify-center w-[16rem] m-4">
-            <img src="public/connect.svg" alt="" className="w-12 h-12 mr-2" />
+            <img src="connect.svg" alt="" className="w-12 h-12 mr-2" />
             <p>
               <span className="font-semibold">CONNECT</span> with the resources
               to meet your business needs
@@ -30,7 +30,7 @@ function Features() {
           </div>
 
           <div className="flex items-center justify-center w-[16rem] m-4">
-            <img src="public/research.svg" alt="" className="w-12 h-12 mr-2" />
+            <img src="research.svg" alt="" className="w-12 h-12 mr-2" />
             <p>
               <span className="font-semibold">RESEARCH</span> and generate
               reports that drive growth
@@ -38,7 +38,7 @@ function Features() {
           </div>
 
           <div className="flex items-center justify-center w-[16rem] m-4">
-            <img src="public/academy.svg" alt="" className="w-12 h-12 mr-2" />
+            <img src="academy.svg" alt="" className="w-12 h-12 mr-2" />
             <p>
               <span className="font-semibold">ACADEMY</span> to give you the
               skills for success in your career
@@ -50,26 +50,22 @@ function Features() {
         <div className="flex justify-between items-center">
           <div className="flex flex-col gap-4">
             <div className="flex flex-col gap-2 ">
-              <img
-                src="public/features-1.svg"
-                className="ml-12"
-                alt="Image 1"
-              />
-              <img src="public/features-2.svg" className="ml-8" alt="Image 1" />
-              <img src="public/features-3.svg" className="ml-4" alt="Image 1" />
+              <img src="features-1.svg" className="ml-12" alt="Image 1" />
+              <img src="features-2.svg" className="ml-8" alt="Image 1" />
+              <img src="features-3.svg" className="ml-4" alt="Image 1" />
             </div>
             <div className="flex flex-col gap-2 relative ">
-              <img src="public/features-4.svg" alt="Image 1" />
+              <img src="features-4.svg" alt="Image 1" />
               <img
-                src="public/features-5.svg"
+                src="features-5.svg"
                 alt="Image 1"
                 className="ml-[4rem] w-[8rem] absolute right-0 top-12"
               />
             </div>
           </div>
-          <img src="public/features-7.svg" alt="Image 1" />
+          <img src="features-7.svg" alt="Image 1" />
         </div>
-        <img src="public/features-6.svg" className="mb-12" alt="Image 2" />
+        <img src="features-6.svg" className="mb-12" alt="Image 2" />
       </div>
     </div>
   );
